3e130ae983ff3adbe80bc7fa298b0a76c7235859
Baptiste Dumont <b.dumont@declic-logiciel.fr>
2018-11-07 10:35:46 +0100

    Update fixtures users + override devise

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_11_06_161524) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "firstname"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_161524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_admin", default: false
+    t.boolean "is_guest", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
